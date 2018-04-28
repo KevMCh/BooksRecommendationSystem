9bd895123aded732063a63a4f86ad74ad77c4ee9
Kevin <marchinkev@gmail.com>
2018-04-29 00:26:38 +0100

    Template

--- ui.R
+++ ui.R
@@ -12,36 +12,48 @@ genreList <- c("Adventure",
                "ScienceFiction"
                )
 
-shinyUI(fluidPage(
-  includeMarkdown('README.md'),
-  
-  fluidRow(
-    column(
-      3,
-      h3("Select Book Genres You Prefer:"),
-      wellPanel(
-        selectInput("inputGenre", "Genre",
-                    genreList)
+shinyUI(htmlTemplate(
+  "www/startbootstrap-freelancer-gh-pages/index.html",
+  title = "Recommender System",
+  content = tabsetPanel(
+    tabPanel(
+      "Doc",
+      style = "margin-top: 30px",
+      includeMarkdown('README.md')
+    ),
+    
+    tabPanel(
+      "Recommender",
+      style = "margin-top: 30px",
+      fluidRow(
+        column(
+          3,
+          h3("Select Book Genres You Prefer:"),
+          wellPanel(
+            selectInput("inputGenre", "Genre",
+                        genreList)
+          )
+        ),
+          
+        column(
+          6,
+          h3("Select Books You Like of these Genres:"),
+          wellPanel(
+            uiOutput("ui")
+          )
+        ),
+        
+        column(
+          3,
+          img(src="images/books.jpg", width="50%")
+        ),
+          
+        column(
+          12,
+          h3("Recommended books:"),
+          tableOutput("table")
+        )
       )
-    ),
-    
-    column(
-      6,
-      h3("Select Books You Like of these Genres:"),
-      wellPanel(
-        uiOutput("ui")
-      )
-    ),
-    
-    column(
-      3,
-      img(src="books.jpg", width="50%")
-    ),
-    
-    column(
-      12,
-      h3("Recommended books:"),
-      tableOutput("table")
     )
   )
 ))
